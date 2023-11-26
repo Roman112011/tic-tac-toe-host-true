d428abf1df5a363a0a16fb24c9f465a1cd8dc58f
unknown <romchikgoro@gmail.com>
2023-11-26 19:16:04 +0200

    new files

---
+++ tic-tac-toe.js
@@ -0,0 +1,120 @@
+let exe = true;
+let nole = false;
+
+document.getElementById('first').onclick = function () {
+    if (exe == true) {
+        document.getElementById('firsttext').innerHTML="x";
+        document.getElementById('onetext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('firsttext').innerHTML="o";
+        document.getElementById('onetext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('second').onclick = function () {
+    if (exe == true) {
+        document.getElementById('secondtext').innerHTML="x";
+        document.getElementById('twotext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('secondtext').innerHTML="o";
+        document.getElementById('twotext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('third').onclick = function () {
+    if (exe == true) {
+        document.getElementById('thirdtext').innerHTML="x";
+        document.getElementById('threetext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('thirdtext').innerHTML="o";
+        document.getElementById('threetext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('fourth').onclick = function () {
+    if (exe == true) {
+        document.getElementById('fourthtext').innerHTML="x";
+        document.getElementById('fourtext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('fourthtext').innerHTML="o";
+        document.getElementById('fourtext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('fiveth').onclick = function () {
+    if (exe == true) {
+        document.getElementById('fivethtext').innerHTML="x";
+        document.getElementById('fivetext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('fivethtext').innerHTML="o";
+        document.getElementById('fivetext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('sixth').onclick = function () {
+    if (exe == true) {
+        document.getElementById('sixthtext').innerHTML="x";
+        document.getElementById('sixtext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('sixthtext').innerHTML="o";
+        document.getElementById('sixtext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('seventh').onclick = function () {
+    if (exe == true) {
+        document.getElementById('seventhtext').innerHTML="x";
+        document.getElementById('seventext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('seventhtext').innerHTML="o";
+        document.getElementById('seventext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('eight').onclick = function () {
+    if (exe == true) {
+        document.getElementById('eighttext').innerHTML="x";
+        document.getElementById('eightttext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('eighttext').innerHTML="o";
+        document.getElementById('eightttext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
+document.getElementById('nineth').onclick = function () {
+    if (exe == true) {
+        document.getElementById('ninethtext').innerHTML="x";
+        document.getElementById('ninetext').innerHTML="ходять O";
+        exe = false;
+        nole = true;
+    }else if(nole == true){
+        document.getElementById('ninethtext').innerHTML="o";
+        document.getElementById('ninetext').innerHTML="ходять X";
+        exe = true;
+        nole = false;
+    }
+}
